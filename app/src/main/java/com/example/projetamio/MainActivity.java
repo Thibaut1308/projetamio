73056823e4b9cb8a8bf4655219a34472344b89c5
Thibaut Wojciak <thibaut.wojciak@telecomnancy.eu>
2023-11-14 18:27:53 +0100

    Fin TP2 début TP3

--- app/src/main/java/com/example/projetamio/MainActivity.java
+++ app/src/main/java/com/example/projetamio/MainActivity.java
@@ -1,21 +1,21 @@
 package com.example.projetamio;
 
 import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
@@ -34,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String PREF_CHECKBOX_STATE = "checkbox_state"; // Clé pour sauvegarder l'état de la CheckBox
     private Intent mainServiceIntent;
     private Intent lightBroadcastReceiver;
+
+    private int notifyId = 0;
     public Map<String, Double> previousLights = new HashMap<>();
 
     @Override
@@ -41,6 +43,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mainServiceIntent = new Intent(this, MainService.class);
         setContentView(R.layout.activity_main);
+        NotificationChannel channel = new NotificationChannel("LIGHTS_CHANNEL", "Changement de luminosité", NotificationManager.IMPORTANCE_DEFAULT);
+        channel.setDescription("Lumonisity channel");
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
         this.registerListener();
         this.registerBroadcastReceiver();
 
@@ -55,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
     public void registerListener() {
         ToggleButton tb1 = findViewById(R.id.BTN1);
         TextView tv2 = findViewById(R.id.TV2);
-        // Button btnRequete = findViewById(R.id.STATICBTN1);
         CheckBox cbStartAtBoot = findViewById(R.id.CBSTARTATBOOT);
 
         tb1.setOnCheckedChangeListener((buttonView, isChecked) -> {
@@ -99,8 +104,9 @@ public class MainActivity extends AppCompatActivity {
                         // Enregistrement/comparatifs des valeurs précédentes
                         if (previousLights.containsKey(l.getMote())) {
                             double previousValue = previousLights.get(l.getMote());
-                            if (Math.abs(l.getValue() - previousValue) < 50) {
+                            if (Math.abs(l.getValue() - previousValue) > 50) {
                                 showNotification("Changement de luminosité", "La lumière du mote " + l.getMote() + " a changée de manière significative");
+                                sendEmail("Changement de luminosité", "La lumière du mote " + l.getMote() + " a changée de manière significative");
                             }
                         }
                         previousLights.put(l.getMote(), l.getValue());
@@ -126,15 +132,33 @@ public class MainActivity extends AppCompatActivity {
         }, light_broadcast_filters);
     }
 
+    @SuppressLint("MissingPermission")
     private void showNotification(String title, String content) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "channel_id")
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "LIGHTS_CHANNEL")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle(title)
                 .setContentText(content)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
-            notificationManager.notify(1, builder.build());
-        }
+        notificationManager.notify(++notifyId, builder.build());
+    }
+
+    @SuppressLint("IntentReset")
+    public void sendEmail(String sujet, String corps) {
+        String destinataire = "test@yopmail.com";
+
+        // Créez une intention ACTION_SEND
+        Intent intent = new Intent(Intent.ACTION_SEND);
+
+
+        intent.setData(Uri.parse("mail:to"));
+        intent.setType("text/plain");
+
+        // Ajoutez l'adresse e-mail du destinataire, le sujet et le corps du message
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{destinataire});
+        intent.putExtra(Intent.EXTRA_SUBJECT, sujet);
+        intent.putExtra(Intent.EXTRA_TEXT, corps);
+
+        startActivity(Intent.createChooser(intent,"Send mail..."));
     }
 }
